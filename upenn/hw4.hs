264c0f134265e947253da509472f9987a6ce4250
Justin Oblak <jroblak@gmail.com>
2014-04-08 17:06:11 -0400

    finished with 4

--- upenn/hw4.hs
+++ upenn/hw4.hs
@@ -1,6 +1,8 @@
 -- http://www.seas.upenn.edu/~cis194/hw/04-higher-order.pdf
+import Data.List (sort)
+
 fun1' :: [Integer] -> Integer
-fun1' = product . map (\x -> x - 2) . filter (even) 
+fun1' = product . map (subtract 2) . filter (even) 
 
 fun2' :: Integer -> Integer
 fun2' = sum . filter (even) . takeWhile (> 1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1) 
@@ -9,12 +11,23 @@ data Tree a = Leaf | Node Integer (Tree a) a (Tree a) deriving (Show, Eq)
 
 -- generates balanced binary tree from list of values using
 -- foldr (integer is height)
-foldTree :: [a] -> Tree a
+foldTree :: (Ord a) => [a] -> Tree a
 foldTree = foldr insertTree Leaf
 
-insertTree :: a -> Tree a -> Tree a
-insertTree x (Node h l val r) = Leaf
-insertTree x Leaf = Leaf
+insertTree :: (Ord a) => a -> Tree a -> Tree a
+insertTree x tree@(Node h l val r)
+	| isBalanced tree = Node (h+1) (insertTree x l) val r
+	| isBalanced l    = Node h l val (insertTree x r)
+	| otherwise       = Node h (insertTree x l) val r
+insertTree x Leaf = Node 0 Leaf x Leaf
+
+height :: Tree a -> Integer
+height Leaf = -1
+height (Node h _ _ _) = h
+
+isBalanced :: Tree a -> Bool
+isBalanced Leaf = True
+isBalanced (Node _ l _ r) = (height l == height r) && (isBalanced l) && (isBalanced r)
 
 -- returns True if and only if there are an odd number of True values
 -- in the input list
