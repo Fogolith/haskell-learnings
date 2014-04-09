7e7a1adb3787448cfc10fed9ce6b4035d50be4eb
Justin Oblak <jroblak@gmail.com>
2014-04-09 17:11:05 -0400

    stubbing some stuff out

---
+++ upenn/hw5/hw5.hs
@@ -0,0 +1,9 @@
+-- http://www.seas.upenn.edu/~cis194/hw/05-type-classes.pdf
+import ExprT
+import Parser
+
+eval :: ExprT -> Integer
+eval = error "todo"
+
+evalStr :: String -> Maybe Integer
+evalStr = error "todo"
