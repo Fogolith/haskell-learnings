2444a02fb443b9d03c179e1f5f582729b26fce4f
Justin Oblak <jroblak@gmail.com>
2014-04-28 15:51:38 -0400

    hw6 done. extra credit todo

--- upenn/hw6.hs
+++ upenn/hw6.hs
@@ -12,47 +12,49 @@ fibs2 :: [Integer]
 fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
 
 -- Deﬁne a data type of polymorphic streams, Stream
--- Stream MUST be infinite, and have only a "cons" constructo
-data Stream = Cons a (Stream a) deriving (Eq)
-
-{-
+-- Stream MUST be infinite, and have only a "cons" constructor
+data Stream a = Cons a (Stream a) deriving (Eq)
 
 -- works by showing only some preﬁx of a stream (say, the ﬁrst 20 elements)
 instance Show a => Show (Stream a) where
-	show ...
+	show = show . take 20 . streamToList 
 
 -- Write a function to convert a Stream to an inﬁnite list
 streamToList :: Stream a -> [a]
-streamToList = error "todo"
+streamToList (Cons x xs) = x : streamToList xs
 
 -- generates a stream containing inﬁnitely many copies of the
 -- given element
 streamRepeat :: a -> Stream a
-streamRepeat = error "todo"
+streamRepeat a = Cons a (streamRepeat a)
 
 -- applies a function to every element of a Stream
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap = error "todo"
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
 
 -- generates a Stream from a “seed” of type a, which is the
 -- ﬁrst element of the stream, and an “unfolding rule” of type a -> a
 -- which speciﬁes how to transform the seed into a new seed, to be
 -- used for generating the rest of the stream.
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed = error "todo"
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
 
 -- the inﬁnite list of natural numbers 0, 1, 2, . . .
 nats :: Stream Integer
-nats = error "todo"
+nats = streamFromSeed (+ 1) 0
 
 -- corresponds to the ruler function 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4, . . .
 -- where the nth element in the stream (assuming the ﬁrst element
 -- corresponds to n = 1) is the largest power of 2 which evenly
 -- divides n.
-ruler :: Stream Interger
-ruler = error "todo"
+ruler :: Stream Integer
+ruler = streamMap (nthRuler) nats
 
--}
+nthRuler :: Integer -> Integer
+nthRuler x
+	| value == [] = 0
+	| otherwise   = head value
+	where value = dropWhile (\z -> x `mod` (z * 2) /= 0) [y | y <- reverse [1..(x - 1)]]
 
 {-
 
