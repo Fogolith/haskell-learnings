a480f0bb5e8a74f9ee42661cab0da6dd27feb8c4
Justin Oblak <jroblak@gmail.com>
2014-05-21 18:03:08 -0400

    i think? hw8 solved. answers online vary

--- upenn/hw8/hw8.hs
+++ upenn/hw8/hw8.hs
@@ -4,6 +4,7 @@ module Party where
 import Employee
 import Data.Monoid
 import Data.Tree
+import Data.List (sort)
 
 glCons :: Employee -> GuestList -> GuestList
 glCons e@(Emp n f) (GL a gf) = GL (e:a) (f + gf)
@@ -21,29 +22,23 @@ treeFold :: (a -> b -> b) -> b -> Tree a -> b
 treeFold f e (Node r [])  = f r e
 treeFold f e (Node r s) = f r (foldr (flip $ treeFold f) e s)
 
--- FIX, WRONG:
--- have to add boss to both lists and see what has the highest score,
--- as well as the one with the highest score without him
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
-nextLevel boss [] = ((GL [] 0), (GL [] 0))
-nextLevel boss [x] = x
-nextLevel boss xs = foldr (\a acc -> maxTuple a acc) (GL [] 0, GL [] 0) xs 
-	where maxTuple (a, b) (c, d) = (moreFun a c, moreFun b d);
+nextLevel boss xs = (maxWithBoss, maxWithoutBoss)
+	where lists = unzip xs;
+		  maxWithoutBoss = mconcat $ map (uncurry moreFun) xs;
+  		  maxWithBoss = glCons boss $ maxWithoutBoss
 
 maxFun' :: Tree Employee -> (GuestList, GuestList)
-maxFun' (Node e l) = nextLevel e l
+maxFun' (Node e []) = nextLevel e [(mempty, mempty)]
+maxFun' (Node e l) = nextLevel e (map maxFun' l)
 
--- takes a company hierarchy as input and outputs a fun-maximizing
--- guest list. can use testCompany to test
 maxFun :: Tree Employee -> GuestList
-maxFun (Node e l) = uncurry moreFun $ nextLevel e (maxFun' l)
+maxFun = uncurry moreFun . maxFun'
 
--- implemnt main :: IO () so that it read the company.txt (use read to turn
--- it into a Tree Employee) and then prints out a formatted guest list,
--- sorted by first name, which looks like:
---	Total fun: 23923
---	Adam McMann
--- 	Adeline Anselme
--- ...
--- try to separate out "pure" computation from the IO as much as possible
--- i.e. don't let IO creep into helper functions
+formatList :: GuestList -> String
+formatList (GL l f) = "Total fun: " ++ show f ++ "\n" ++ (unlines . sort . map empName $ l)
+
+main :: IO ()
+main = do
+	file <- readFile "company.txt"
+	putStr . formatList . maxFun . read $ file
