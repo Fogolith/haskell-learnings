9df7d1f28026940108697fb9a2a65075237b7556
Justin Oblak <jroblak@gmail.com>
2014-05-27 17:29:03 -0400

    hw10 complete

--- upenn/hw10/hw10.hs
+++ upenn/hw10/hw10.hs
@@ -1,19 +1,11 @@
 -- http://www.seas.upenn.edu/~cis194/hw/10-applicative.pdf
 module AParser where
 
-import           Control.Applicative
-import           Data.Char
+import Control.Applicative
+import Data.Char
 
--- A parser for a value of type a is a function which takes a String
--- represnting the input to be parsed, and succeeds or fails; if it
--- succeeds, it returns the parsed value along with the remainder of
--- the input.
 newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
 
--- For example, 'satisfy' takes a predicate on Char, and constructs a
--- parser which succeeds only if it sees a Char that satisfies the
--- predicate (which it then returns).  If it encounters a Char that
--- does not satisfy the predicate (or an empty input), it fails.
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = Parser f
   where
@@ -24,24 +16,9 @@ satisfy p = Parser f
         | p x       = Just (x, xs)
         | otherwise = Nothing  -- otherwise, fail
 
--- Using satisfy, we can define the parser 'char c' which expects to
--- see exactly the character c, and fails otherwise.
 char :: Char -> Parser Char
 char c = satisfy (== c)
 
-{- For example:
-
-*Parser> runParser (satisfy isUpper) "ABC"
-Just ('A',"BC")
-*Parser> runParser (satisfy isUpper) "abc"
-Nothing
-*Parser> runParser (char 'x') "xyz"
-Just ('x',"yz")
-
--}
-
--- For convenience, we've also provided a parser for positive
--- integers.
 posInt :: Parser Integer
 posInt = Parser f
   where
@@ -50,6 +27,41 @@ posInt = Parser f
       | otherwise = Just (read ns, rest)
       where (ns, rest) = span isDigit xs
 
-------------------------------------------------------------
--- Your code goes below here
-------------------------------------------------------------
+
+first :: (a -> b) -> (a,c) -> (b,c)
+first f p = (f $ fst p, snd p)
+
+instance Functor Parser where
+    fmap f (Parser a) = Parser $ fmap (first f) . a
+
+instance Applicative Parser where
+    pure a  = Parser (\_ -> Just (a,""))
+    a <*> b = Parser f
+        where
+            f s = case (runParser a s) of -- run parser 1
+                Just (x, y) -> case (runParser b y) of -- on success run parser 2
+                    Nothing -> Nothing -- fail on Nothing (p 2)
+                    Just (q, w) -> Just (x q, w) -- apply function returned from p1 on results of p2
+                Nothing -> Nothing -- fail on Nothing (p 1)
+
+-- LEFT-associative, so (,) <$> (char 'a') happens first
+-- which gives us a parser that returns a partially applied (,) function
+-- and the remaining string
+abParser :: Parser (Char, Char)
+abParser = (,) <$> (char 'a') <*> (char 'b')
+
+abParser_ :: Parser ()
+abParser_ = (const . const ()) <$> (char 'a') <*> (char 'b')
+
+intPair :: Parser [Integer]
+intPair = (\x y z -> [x, z]) <$> posInt <*> (char ' ') <*> posInt
+
+instance Alternative Parser where
+    empty   = Parser (\_ -> Nothing)
+    a <|> b = Parser f
+        where
+            f s = case (runParser a s) of Just x -> Just x
+                                          Nothing -> (runParser b s)
+
+intOrUppercase :: Parser ()
+intOrUppercase = (const ()) <$> (satisfy isUpper) <|> (const ()) <$> (posInt)
