e8df62482babc1b6abf6914add9baf1ae1ee6db9
Justin Oblak <jroblak@gmail.com>
2014-06-24 13:14:05 -0400

    more monads

--- typeclassopedia/Monad.hs
+++ typeclassopedia/Monad.hs
@@ -1,4 +1,5 @@
 import Control.Applicative
+import Control.Monad
 
 {- Segue: The Trivial Monad -}
 data W a = W a deriving Show
@@ -70,12 +71,10 @@ instance Monad ((->) e) where
 	a >>= f = (\e -> f (a e)) -- i think
 -}
 
+-- ============ NEED TO REVISIT ============ --
+
 data Free f a = Var a
               | Node (f (Free f a))
-
--- Implement Functor and Monad instances for Free f
--- You may assume that f has a Functor instance. This is 
--- known as the free monad built from the functor f.
 
 instance (Functor f) => Functor (Free f) where
 	fmap f (Var a) = Var (f a)
@@ -83,8 +82,29 @@ instance (Functor f) => Functor (Free f) where
 
 instance (Functor f) => Applicative (Free f) where
 	pure = Var
-	a <*> b = error "todo"
+	(Var f) <*> a = fmap f a
+	(Node a) <*> b = Node (fmap (<*> b) a)
 
 instance (Functor f) => Monad (Free f) where
-	return = error "todo"
-	(>>=) = error "todo"
+	return = Var
+	(Var a) >>= f = f a
+	a >>= f = join (fmap f a)
+
+-- ============ NEED TO REVISIT ============ --
+
+{- 
+Implement (>>=) in terms of fmap (or liftM) and join.
+
+	Had to do this for join, above
+
+Now implement join and fmap (liftM) in terms of (>>=) and return.
+
+	join :: (m (m a)) -> m a
+	join a = a >>= id 
+
+	fmap :: (a -> b) -> f a -> f b 
+	fmap f a = a >>= (return . f)
+
+Given the definition g >=> h = \x -> g x >>= h, prove the equivalence 
+of the above laws and the usual monad laws.
+-}
